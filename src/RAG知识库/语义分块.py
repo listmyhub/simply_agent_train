9184dd10099d671dcc73238bf33239efff0be9e9
listmyhub <1662922137@qq.com>
2026-09-01 13:09:44 +0800

    加入检索部分

--- src/RAG知识库/语义分块.py
+++ src/RAG知识库/语义分块.py
@@ -7,14 +7,14 @@ from langchain_experimental.text_splitter import SemanticChunker
 
 from RAG知识库.load1 import DASHSCOPE_API_KEY
 from RAG知识库.递归分块 import text
-os.environ["DASHSCOPE_API_KEY"] = DASHSCOPE_API_KEY
+# os.environ["DASHSCOPE_API_KEY"] = DASHSCOPE_API_KEY
 embedings=DashScopeEmbeddings(
     model="text-embedding-v4"
 )
 senmatic_split=SemanticChunker(
     embeddings=embedings,
     breakpoint_threshold_type="percentile",
-    breakpoint_threshold_amount=85,
+    breakpoint_threshold_amount=80,
     sentence_split_regex=r"(?<=[。！？])\s*",
     min_chunk_size=30
 )
@@ -29,6 +29,7 @@ chunks=senmatic_split.split_text("""
 从小到大，泡面一直被贴上“不健康、全是防腐剂”的标签，成为大众认知里的“垃圾食品”。
 多年来，防腐剂替泡面稳稳背了数年黑锅。但根据人民日报健康等权威科普辟谣：市面上绝大多数泡面面饼，
 压根不需要添加防腐剂，就能实现长期保质。大家对泡面的固有偏见，其实都是一场流传已久的认知误区。
+我爱吃苹果和香蕉，他们是最好吃的水果
 """)
 for i,chunk in enumerate(chunks):
     print(f"总共分成了{len(chunks)},其中第{i+1}块为：")
